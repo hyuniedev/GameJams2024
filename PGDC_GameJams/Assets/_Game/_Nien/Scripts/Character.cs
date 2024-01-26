6d48561ef173a4cd82be55b2ad08b65237f8afb8
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-01-27 05:39:00 +0700

    add hasSkill Character

--- PGDC_GameJams/Assets/_Game/_Nien/Scripts/Character.cs
+++ PGDC_GameJams/Assets/_Game/_Nien/Scripts/Character.cs
@@ -6,7 +6,7 @@ public class Character : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float forceJump;
-
+    private bool hasSkill;
     public float ForceJump
     {
         get => forceJump;
@@ -16,5 +16,11 @@ public class Character : MonoBehaviour
     {
         get => moveSpeed;
         set => moveSpeed = value;
+    }
+
+    public bool HasSkill
+    {
+        get => hasSkill;
+        set => hasSkill = value;
     }
 }
