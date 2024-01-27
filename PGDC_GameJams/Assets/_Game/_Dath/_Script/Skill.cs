958940bcbf070389a17a54c76739810c124693ac
DatThanhDoan <datthanhdoan.charlie@gmail.com>
2024-01-28 04:11:01 +0700

    fix skill

--- PGDC_GameJams/Assets/_Game/_Dath/_Script/Skill.cs
+++ PGDC_GameJams/Assets/_Game/_Dath/_Script/Skill.cs
@@ -4,57 +4,13 @@ using UnityEngine;
 
 public class Skill : MonoBehaviour
 {
-    private bool isCompareTag = false;
+    protected bool isCompareTag = false;
     [SerializeField] private LayerMask _layerMask;
 
     protected virtual void UseSkill(GameObject player)
     {
     }
 
-    // no deo chay ???
-    protected GameObject GetPlayerInFront(Transform handPos)
-    {
-        // Draw line direction of player
-        RaycastHit2D hit = Physics2D.Raycast(handPos.position, handPos.forward, 50f, _layerMask);
-        Debug.DrawRay(handPos.position, handPos.forward * 50f, Color.red);
-
-        return hit.collider != null ? hit.collider.gameObject : null;
-    }
-
-
-    protected Vector2 GetDirectionOfPlayer(GameObject player)
-    {
-        Vector2 playerDirection = player.transform.forward;
-        return playerDirection;
-    }
-
-    // Draw line direction of player
-    protected void DrawLineDirectionOfPlayer(GameObject player)
-    {
-        Vector2 playerDirection = GetDirectionOfPlayer(player);
-        Debug.DrawRay(player.transform.position, playerDirection * 10, Color.red);
-    }
-
-
-
-    // Todo : Sử dụng trong script của rocket
-
-    protected List<GameObject> GetPlayerInCircle(GameObject player, float radius)
-    {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius);
-        List<GameObject> players = new List<GameObject>();
-
-        foreach (Collider2D collider in colliders)
-        {
-            GameObject playerObject = collider.gameObject;
-            if (playerObject.CompareTag("Player"))
-            {
-                players.Add(playerObject);
-            }
-        }
-
-        return players.Count > 0 ? players : null;
-    }
 
     public void ShockWave(Transform trans, float radius, float time, float boomForce)
     {
@@ -76,7 +32,9 @@ public class Skill : MonoBehaviour
         if (other.gameObject.CompareTag("Player") && !isCompareTag)
         {
             isCompareTag = true;
+            other.GetComponent<IEventHappen>().EventHappen(true);
             UseSkill(other.gameObject);
+            other.GetComponent<IEventHappen>().EventHappen(false);
             Destroy(gameObject);
         }
     }
