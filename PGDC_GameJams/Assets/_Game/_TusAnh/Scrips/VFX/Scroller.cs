60c03560d7c2a7b27c367c9f1dbd1daa87c854b6
Tran Anh Tu <85071582+tanhtuscode@users.noreply.github.com>
2024-01-27 20:40:18 +0700

    update file and scene

--- PGDC_GameJams/Assets/_Game/_TusAnh/Scrips/VFX/Scroller.cs
+++ PGDC_GameJams/Assets/_Game/_TusAnh/Scrips/VFX/Scroller.cs
@@ -7,10 +7,10 @@ namespace _Game.Scripts.GameSystem
     {
         [SerializeField] private RawImage _img;
         [SerializeField] private float _x, _y;
- 
+        [SerializeField] private float speed;
         void Update()
         {
-            _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x,_y) * Time.deltaTime,_img.uvRect.size);               
+            _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x,_y) * Time.deltaTime * speed,_img.uvRect.size);               
         }
     }
     
