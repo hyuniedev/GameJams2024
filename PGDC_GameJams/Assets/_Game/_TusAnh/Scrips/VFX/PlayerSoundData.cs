ac9eceb28eefec391f3063b8b880b2b08331e4aa
Tran Anh Tu <85071582+tanhtuscode@users.noreply.github.com>
2024-01-27 10:11:37 +0700

    Update Project With Title Screen , added SoundManager

---
+++ PGDC_GameJams/Assets/_Game/_TusAnh/Scrips/VFX/PlayerSoundData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
+
+namespace _Game._TusAnh.SOs
+{
+    [CreateAssetMenu(fileName = "SoundData", menuName = "SoundData", order = 0)]
+    public class PlayerSoundData : ScriptableObject
+    {
+        public List<AudioClip> backGroundMusic;
+        public List<AudioClip> sfx;
+        public AudioMixer audioMixer;
+
+        public List<AudioClip> BackGroundMusic
+        {
+            get => backGroundMusic;
+            set => backGroundMusic = value;
+        }
+
+        public List<AudioClip> Sfx
+        {
+            get => sfx;
+            set => sfx = value;
+        }
+
+        public AudioMixer AudioMixer
+        {
+            get => audioMixer;
+            set => audioMixer = value;
+        }
+    }
+}
