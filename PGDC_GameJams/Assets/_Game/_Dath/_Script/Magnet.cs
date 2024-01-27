69ece24eaddddd2e0ed523cea0785bc409c775e3
DatThanhDoan <datthanhdoan.charlie@gmail.com>
2024-01-27 17:16:16 +0700

    Update Skill Magnet
    Skill Magnet chạy được với Player bình thường (LorPlayer) nhưng không chạy được với Player là prefab của Niên

--- PGDC_GameJams/Assets/_Game/_Dath/_Script/Magnet.cs
+++ PGDC_GameJams/Assets/_Game/_Dath/_Script/Magnet.cs
@@ -7,10 +7,11 @@ using UnityEngine;
 public class Magnet : MonoBehaviour
 {
     public bool facingRight = true;
-    float _radius = 15f;
-    float _strength = 1f;
+    public float boundInsideRadius = 8f;
+    float _radius = 25f;
+    float _strength = 15f;
     float _maxStrength = 5f;
-
+    Vector2 testDirection;
     private void Update()
     {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, _radius);
@@ -20,20 +21,41 @@ public class Magnet : MonoBehaviour
 
             if (playerVictim.CompareTag("Player") && !transform.IsChildOf(playerVictim.transform))
             {
+                Debug.Log(playerVictim.name);
+                float boundInside = Vector2.Distance(transform.position, playerVictim.transform.position);
+                if (boundInside < boundInsideRadius) return;
                 float checkPos = playerVictim.transform.position.x - transform.position.x;
                 Rigidbody2D rb = playerVictim.GetComponent<Rigidbody2D>();
                 Vector2 direction = transform.position - playerVictim.transform.position;
+
+
+                // Test
+                testDirection = direction;
+                // -- Test
                 direction.Normalize();
 
                 if (facingRight)
                 {
+                    Debug.Log("facingRight");
                     float distanceX = playerVictim.transform.position.x - transform.position.x;
-                    if (distanceX > 0) rb.velocity = direction * _strength;
+                    if (distanceX > 0)
+                    {
+
+                        rb.velocity = direction * _strength;
+                        Debug.Log("distanceX > 0");
+                        Debug.Log(rb.velocity);
+                    }
                 }
                 if (!facingRight)
                 {
+                    Debug.Log("facingLeft");
                     float distanceX = playerVictim.transform.position.x - transform.position.x;
-                    if (distanceX < 0) rb.velocity = direction * _strength;
+                    if (distanceX < 0)
+                    {
+                        Debug.Log("distanceX < 0");
+                        rb.velocity = direction * _strength;
+                        Debug.Log(rb.velocity);
+                    }
                 }
 
             }
@@ -44,7 +66,8 @@ public class Magnet : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, _radius);
-
+        Gizmos.DrawWireSphere(transform.position, boundInsideRadius);
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(testDirection.x, testDirection.y, 0));
     }
 
 }
